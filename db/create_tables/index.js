c89b1c3ae8c48dc7ae2925656ff5afd4ac66b3ae
Ricardo Rodriguez <tjrprecursor@gmail.com>
2019-03-16 12:53:18 -0400

    indixing create tables queries

---
+++ db/create_tables/index.js
@@ -0,0 +1,33 @@
+exports.createAttributes = require('./createAttributes');
+
+exports.createAttributeValue = require('./createAttributeValue');
+
+exports.createAudit = require('./createAudit');
+
+exports.createCategory = require('./createCategory');
+
+exports.createCustomer = require('./createCustomer');
+
+exports.createCustomer = require('./createCustomer');
+
+exports.createDeparments = require('./createDeparments');
+
+exports.createOrderDetails = require('./createOrderDetails');
+
+exports.createOrders = require('./createOrders');
+
+exports.createProduct = require('./createProduct');
+
+exports.createProductAttribute = require('./createProductAttribute');
+
+exports.createProductCategory = require('./createProductCategory');
+
+exports.createReview = require('./createReview');
+
+exports.createShipping = require('./createShipping');
+
+exports.createShippingRegion = require('./createShippingRegion');
+
+exports.createShoppingCart = require('./createShoppingCart');
+
+exports.createTax = require('./createTax');
