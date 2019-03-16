eda1fe0fae7fe1abf68f5f9eb3771a57e393f3f8
Ricardo Rodriguez <tjrprecursor@gmail.com>
2019-03-16 13:54:52 -0400

    minor fix

--- db/init.js
+++ db/init.js
@@ -2,3 +2,45 @@
  * WITH THIS PROCESS YOU CAN INITIALIZE THE ECOMMERCE DATABASE
  * IF YOU WANT TO RESET DATABASE YOU MUST DOING MANUALLY BY YOURSELF
  */
+// import DB connection with sequelize
+const sequelize = require('../common/connection');
+
+const {
+  createAttributeValue,
+  createAttributes,
+  createAudit,
+  createCategory,
+  createCustomer,
+  createDeparments,
+  createOrderDetails,
+  createOrders,
+  createProduct,
+  createProductAttribute,
+  createProductCategory,
+  createReview,
+  createShipping,
+  createShippingRegion,
+  createShoppingCart,
+  createTax,
+} = require('./create_tables');
+
+const init = async () => {
+  await sequelize.query(createDeparments);
+  await sequelize.query(createCategory);
+  await sequelize.query(createProduct);
+  await sequelize.query(createProductCategory);
+  await sequelize.query(createAttributes);
+  await sequelize.query(createAttributeValue);
+  await sequelize.query(createProductAttribute);
+  await sequelize.query(createShoppingCart);
+  await sequelize.query(createOrders);
+  await sequelize.query(createOrderDetails);
+  await sequelize.query(createShippingRegion);
+  await sequelize.query(createCustomer);
+  await sequelize.query(createShipping);
+  await sequelize.query(createTax);
+  await sequelize.query(createAudit);
+  await sequelize.query(createReview);
+};
+
+init();
