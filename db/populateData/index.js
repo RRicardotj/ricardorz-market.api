fae32cfb5ce8452ed850bc36aa29b5282e9cda61
Ricardo Rodriguez <tjrprecursor@gmail.com>
2019-03-16 13:23:48 -0400

    indexing populate data

---
+++ db/populateData/index.js
@@ -0,0 +1,17 @@
+exports.attributeData = require('./attributeData');
+
+exports.categoryData = require('./categoryData');
+
+exports.deparmentData = require('./deparmentData');
+
+exports.productAttributeData = require('./productAttributeData');
+
+exports.productCategory = require('./productCategory');
+
+exports.productData = require('./productData');
+
+exports.shipping = require('./shipping');
+
+exports.shippingRegionData = require('./shippingRegionData');
+
+exports.taxData = require('./taxData');
